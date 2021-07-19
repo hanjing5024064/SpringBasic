6243d66b56019351b22b0fc371eb3a8f81f4444e
zhangjun <5024064@qq.com>
2021-07-19 22:50:18 +0800

    注解方式引入xml、测试类、bean： @ContextConfiguration(locations = { "classpath:spring/spring-base.xml" })	// 进行资源文件定位 @RunWith(SpringJUnit4ClassRunner.class)	// 设置要使用的测试工具 @Resource(name="messageImpl")	// 根据配置文件名称注入

--- mldnspring-base/src/test/java/cn/mldn/mldnspring/TestMessageDemo.java
+++ mldnspring-base/src/test/java/cn/mldn/mldnspring/TestMessageDemo.java
@@ -1,17 +1,21 @@
 package cn.mldn.mldnspring;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.mldn.mldnspring.service.IMessage;
-
+@ContextConfiguration(locations = { "classpath:spring/spring-base.xml" })	// 进行资源文件定位
+@RunWith(SpringJUnit4ClassRunner.class)	// 设置要使用的测试工具
 public class TestMessageDemo {
-
-	public static void main(String[] args) {
-		// 启动Spring上下文，在以后的实际开发过程之中，这一启动过程会由WEB容器负责
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-base.xml");
-		IMessage message = ctx.getBean("messageImpl", IMessage.class); // 获取实例化对象
-		System.out.println(message.echo("www.mldn.cn"));
+	@Resource(name="messageImpl")	// 根据配置文件名称注入
+	private IMessage message ;		// 会自动根据类型进行注入
+	@Test
+	public void testEcho() {
+		System.out.println(this.message.echo("www.mldn.cn")); 
 	}
 
 }
