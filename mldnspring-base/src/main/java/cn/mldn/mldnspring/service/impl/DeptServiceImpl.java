5f65eb48a0e0b0b6a76d5fca76cdd9b034361802
zhangjun <5024064@qq.com>
2021-07-24 11:56:21 +0800

    Bean管理：3.8 基于Annotation配置管理：类型冲突，无法自动注入时，通过@Primary指定优先选择配置

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/service/impl/DeptServiceImpl.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/service/impl/DeptServiceImpl.java
@@ -3,13 +3,15 @@ package cn.mldn.mldnspring.service.impl;
 import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import cn.mldn.mldnspring.dao.IDeptDAO;
 import cn.mldn.mldnspring.service.IDeptService;
 import cn.mldn.mldnspring.vo.Dept;
 @Service
-public class DeptServiceImpl implements IDeptService {
+@Primary
+public class DeptServiceImpl implements IDeptService { 
 	@Autowired					// 自动注入
 	private IDeptDAO deptDAO ; 
 	@Override
