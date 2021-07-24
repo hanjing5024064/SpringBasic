426510469057fcc383000f14b7a9f45850c1ad92
zhangjun <5024064@qq.com>
2021-07-24 11:03:19 +0800

    Bean管理：3.5 Bean的实例化管理：默认饿汉模式，Spring启动时就实例化各Bean；添加lazy-init="true"，改为懒汉模式，只有使用到时才实例化；Spring处理了懒汉设计模式中的多线程同步问题。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/StartApplication.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/StartApplication.java
@@ -8,15 +8,15 @@ import cn.mldn.mldnspring.vo.Dept;
 public class StartApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-base.xml");
-		for (int x = 0 ; x < 3 ; x ++) {
-			int temp = x ; 
-			new Thread(()->{
-				Dept dept = ctx.getBean("dept",Dept.class) ;	// 获得程序类
-				dept.setDeptno(dept.getDeptno() + temp);			// 修改部门信息
-				dept.setDname(dept.getDname() + " - " + temp);	// 修改部门信息
-				System.out.println(Thread.currentThread().getName() + "、dept = " + dept);
-			}) .start() ;
-		}
+//		for (int x = 0 ; x < 3 ; x ++) {
+//			int temp = x ; 
+//			new Thread(()->{
+//				Dept dept = ctx.getBean("dept",Dept.class) ;	// 获得程序类
+//				dept.setDeptno(dept.getDeptno() + temp);			// 修改部门信息
+//				dept.setDname(dept.getDname() + " - " + temp);	// 修改部门信息
+//				System.out.println(Thread.currentThread().getName() + "、dept = " + dept);
+//			}) .start() ;
+//		}
 	}
 
 }
