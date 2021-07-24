824787d288b57ce2c781cc6250846bd097c19007
zhangjun <5024064@qq.com>
2021-07-24 10:56:45 +0800

    Bean管理：3.5 Bean的实例化管理：多线程修改bean验证单列模式

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/StartApplication.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/StartApplication.java
@@ -3,9 +3,20 @@ package cn.mldn.mldnspring;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import cn.mldn.mldnspring.vo.Dept;
+ 
 public class StartApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-base.xml");
+		for (int x = 0 ; x < 3 ; x ++) {
+			int temp = x ; 
+			new Thread(()->{
+				Dept dept = ctx.getBean("dept",Dept.class) ;	// 获得程序类
+				dept.setDeptno(dept.getDeptno() + temp);			// 修改部门信息
+				dept.setDname(dept.getDname() + " - " + temp);	// 修改部门信息
+				System.out.println(Thread.currentThread().getName() + "、dept = " + dept);
+			}) .start() ;
+		}
 	}
 
 }
