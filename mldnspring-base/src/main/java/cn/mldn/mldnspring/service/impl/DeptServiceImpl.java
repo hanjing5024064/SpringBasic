865c605c10dfef9524cdd5cd9c5c61d178c03234
zhangjun <5024064@qq.com>
2021-07-24 11:53:43 +0800

    Bean管理：3.8 基于Annotation配置管理：@Autowired注解替代@Resource @Resource = @Autowired + @Qualifier 默认两者会根据类型自动匹配注入对象。类型相同时可以通过名称byName进行匹配，@Resource可直接通过那么属性设置Bean名称，@Autowired必须结合@Qualifier注解来设置名称。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/service/impl/DeptServiceImpl.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/service/impl/DeptServiceImpl.java
@@ -2,6 +2,7 @@ package cn.mldn.mldnspring.service.impl;
 
 import javax.annotation.Resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import cn.mldn.mldnspring.dao.IDeptDAO;
@@ -9,8 +10,8 @@ import cn.mldn.mldnspring.service.IDeptService;
 import cn.mldn.mldnspring.vo.Dept;
 @Service
 public class DeptServiceImpl implements IDeptService {
-	@Resource					// 自动注入
-	private IDeptDAO deptDAO ;
+	@Autowired					// 自动注入
+	private IDeptDAO deptDAO ; 
 	@Override
 	public boolean add(Dept vo) {
 		System.out.println("************ 执行业务层方法 ************");
