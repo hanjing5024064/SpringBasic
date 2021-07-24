8eca24ebb18ea60619c7a8e0110f38052139e717
zhangjun <5024064@qq.com>
2021-07-24 10:41:10 +0800

    Bean管理：3.5 自动匹配-根据类型：emp没有定义dept的属性，指定根据类型匹配autowire="byType"，所以可以自动找到匹配

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Dept.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Dept.java
@@ -1,17 +1,11 @@
 package cn.mldn.mldnspring.vo;
 
-import java.beans.ConstructorProperties;
 import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Dept implements Serializable {
 	private Long deptno ;
 	private String dname ;
-	@ConstructorProperties(value= {"paramDeptno","paramDname"})
-	public Dept(Long deptno,String dname) {
-		this.deptno = deptno ;
-		this.dname = dname ;
-	}
 	// setter、getter、toString()略
 	public Long getDeptno() {
 		return deptno;
--- mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Emp.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Emp.java
@@ -7,9 +7,6 @@ import java.util.Date;
 public class Emp implements Serializable {
 	private Long empno ;
 	private String ename ;
-	private Integer age ;
-	private Double salary ;
-	private Date hiredate ; 
 	private Dept dept ;
 	// setter、getter、toString()略
 	public Long getEmpno() {
@@ -24,33 +21,14 @@ public class Emp implements Serializable {
 	public void setEname(String ename) {
 		this.ename = ename;
 	}
-	public Integer getAge() {
-		return age;
-	}
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-	public void setSalary(Double salary) {
-		this.salary = salary;
-	}
-	public Double getSalary() {
-		return salary;
-	}
-	public Date getHiredate() {
-		return hiredate;
-	}
-	public void setHiredate(Date hiredate) {
-		this.hiredate = hiredate;
+	public Dept getDept() {
+		return dept;
 	}
 	public void setDept(Dept dept) {
 		this.dept = dept;
 	}
-	public Dept getDept() {
-		return dept;
-	}
 	@Override
 	public String toString() {
-		return "Emp [empno=" + empno + ", ename=" + ename + ", age=" + age + ", salary=" + salary + ", hiredate="
-				+ hiredate + "]";
+		return "Emp [empno=" + empno + ", ename=" + ename + "]";
 	}
 }
