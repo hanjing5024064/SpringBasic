cd51b0f882370bfb246832967bea259200726ec3
zhangjun <5024064@qq.com>
2021-07-25 10:55:25 +0800

    通过ResourceLoader接口：通过资源定位字符串区分加载子类，将具体子类与代码解耦。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/resourcedemo/ClassPathResourceDemo.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/resourcedemo/ClassPathResourceDemo.java
@@ -2,13 +2,16 @@ package cn.mldn.mldnspring.resourcedemo;
 
 import java.util.Scanner;
 
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 public class ClassPathResourceDemo { 
 	public static void main(String[] args) throws Exception {
+		ResourceLoader resourceLoader = new DefaultResourceLoader() ;
 		// 创建了一个文件读取的Resource资源对象
-		Resource resource = new ClassPathResource("spring/spring-base.xml") ;
+		Resource resource = resourceLoader.getResource("classpath:spring/spring-base.xml") ;
+
 		System.out.println("资源长度：" + resource.contentLength());
 		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
 		Scanner scan = new Scanner(resource.getInputStream()) ;
--- mldnspring-base/src/main/java/cn/mldn/mldnspring/resourcedemo/FileSystemResourceDemo.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/resourcedemo/FileSystemResourceDemo.java
@@ -1,17 +1,16 @@
 package cn.mldn.mldnspring.resourcedemo;
 
-import java.io.File;
 import java.util.Scanner;
 
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 public class FileSystemResourceDemo {
 	public static void main(String[] args) throws Exception {
-		// 创建了一个文件读取的Resource资源对象
-		Resource resource = new FileSystemResource(new File("D:" + File.separator + "mldn.txt")) ;
+		ResourceLoader resourceLoader = new DefaultResourceLoader() ;
+		Resource resource = resourceLoader.getResource("file:d:/mldn.txt") ; // 文件资源
 		System.out.println("资源长度：" + resource.contentLength());
-		// 读取内容可以使用Resource父接口InputStreamSrouce提供的getInputStream()方法来完成
 		Scanner scan = new Scanner(resource.getInputStream()) ;
 		while (scan.hasNext()) {
 			System.out.print(scan.next()); 
