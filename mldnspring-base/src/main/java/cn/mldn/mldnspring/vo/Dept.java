b1e3f318453448cfdfe12b838527884b10610f26
zhangjun <5024064@qq.com>
2021-07-25 12:53:52 +0800

    Spring表达式语言SpEL: 5.8 Spring配置文件与SpEL：基于Annotation使用SpEL

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Dept.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/vo/Dept.java
@@ -1,27 +1,43 @@
 package cn.mldn.mldnspring.vo;
 
-import java.io.Serializable;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-@SuppressWarnings("serial")
-public class Dept implements Serializable {
+@Component
+public class Dept {
+	@Value("#{item.createDate?.time}")
 	private Long deptno ;
+	@Value("#{item.title.toUpperCase()}")
 	private String dname ;
-	// setter、getter、toString()略
+	@Value("#{'中国北京'.substring(2,4)}")
+	private String loc ;
 	public Long getDeptno() {
 		return deptno;
 	}
+
 	public void setDeptno(Long deptno) {
 		this.deptno = deptno;
 	}
+
 	public String getDname() {
 		return dname;
 	}
+
 	public void setDname(String dname) {
 		this.dname = dname;
 	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
 	@Override
 	public String toString() {
-		return "部门编号：" + this.deptno + "、部门名称：" + this.dname ;
+		return "Dept [deptno=" + deptno + ", dname=" + dname + ", loc=" + loc + "]";
 	}
 	
 }
--- mldnspring-base/src/test/java/cn/mldn/mldnspring/TestDept.java
+++ mldnspring-base/src/test/java/cn/mldn/mldnspring/TestDept.java
@@ -1,20 +1,20 @@
 package cn.mldn.mldnspring;
 
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.mldn.mldnspring.vo.Dept;
-@ContextConfiguration(locations = { "classpath:spring/spring-base.xml" })	// 进行资源文件定位
+
+@ContextConfiguration(locations = { "classpath:spring/spring-base.xml" })	// 资源文件定位
 @RunWith(SpringJUnit4ClassRunner.class)	// 设置要使用的测试工具
 public class TestDept {
-	@Resource
-	private Dept dept ;		// 注入对象
+	@Autowired
+	private Dept dept ;
 	@Test
-	public void testBean() {
-		System.out.println("部门编号：" + this.dept.getDeptno() + "、部门名称：" + this.dept.getDname());
+	public void testItem() {
+		System.out.println(this.dept);
 	}
-} 
+}
