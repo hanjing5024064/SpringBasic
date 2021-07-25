8c3f5beb23fab74e1cd8bfd3383eaa07cc3c89f6
zhangjun <5024064@qq.com>
2021-07-25 12:09:49 +0800

    Spring表达式语言SpEL: 5.2原理：标记识别、内容转换、上下文变量环境、计算结果...

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/spel/SpELFirstDemo.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/spel/SpELFirstDemo.java
@@ -8,13 +8,11 @@ import org.springframework.expression.spel.support.StandardEvaluationContext;
 
 public class SpELFirstDemo {
 	public static void main(String[] args) {
-		String str = "(\"Hello \" + \"World !!!\").substring(#start,#end)" ;	// 表达式定义
+		String str = "10 + 20" ;	// 表达式字符串
 		ExpressionParser parser = new SpelExpressionParser() ;		// Spring表达式解析器
 		Expression exp = parser.parseExpression(str) ;				// 解析表达式
 		EvaluationContext context = new StandardEvaluationContext() ;	// 上下文计算
-		context.setVariable("start", 6); 							// 定义变量
-		context.setVariable("end", 12);								// 定义变量
-		System.out.println(exp.getValue(context));					// 表达式计算
-
+		Integer result = (Integer) exp.getValue(context) ;				// 表达式计算
+		System.out.println(result);	
 	}
 }
