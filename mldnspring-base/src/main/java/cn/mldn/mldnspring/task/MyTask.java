4a159d2b11088f00dfdf34409a917a876a0e66a1
zhangjun <5024064@qq.com>
2021-08-01 10:34:30 +0800

    SpringTask(轻量级QuartZ): 6.3.2 基于Annotation的SpringTask配置 1. 定义任务类及方法：注解@Component,@Scheduled(cron="...")/@Scheduled(fixedDelay="")实现间隔/定时 2. 配置文件定义自动加载包内组件及启用Annotation的SpringTask配置：<task:annotation-driven/>

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
@@ -2,8 +2,12 @@ package cn.mldn.mldnspring.task;
 
 import java.text.SimpleDateFormat;
 
-public class MyTask {			// 不再强制性继承任何父类
-	public void runTask() { 	// 随意定义的一个方法名称
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Component
+public class MyTask {
+	@Scheduled(cron="* * * * * ?")	// 任务调度设置
+	public void runTask() { 		// 随意定义了一个方法名称
 		System.out.println("【当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
 	}
 }
