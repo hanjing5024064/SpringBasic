8e5aa8bb2293b9fc2ce1f54495c6e20050cfd325
zhangjun <5024064@qq.com>
2021-08-01 15:26:26 +0800

    AOP切面编程：7.4 AOP基础实现-前置通知参数接收 1. 编写关注点处理类及方法（前置方法加入传参） 2. 配置文件中织入处理，传入参数 			<aop:before method="handleBefore" 				pointcut="execution(public * cn.mldn..service..*.*(..)) and args(msg)" 				arg-names="msg"/>				<!-- 定义新的切入点，将参数传入处理前置通知 -->

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/advice/ServiceAdvice.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/advice/ServiceAdvice.java
@@ -1,8 +1,8 @@
 package cn.mldn.mldnspring.advice;
 
 public class ServiceAdvice {		// 该类不需要继承任何父类，独立存在
-	public void handleBefore() { 	// 处理前置通知
-		System.out.println("【### ServiceAdvice-handleBefore ###】进行业务的前置处理操作。");
+	public void handleBefore(String tempMsg) { 	// 处理前置通知
+		System.out.println("【### ServiceAdvice-handleBefore ###】进行业务的前置处理操作，参数：" + tempMsg);
 	}
 	public void handleAfter() { 	// 处理后置操作通知
 		System.out.println("【### ServiceAdvice-handleAfter ###】进行业务的后置处理操作。");
