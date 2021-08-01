ba0485d4bfad2cbc9ea60e3ad3c484c681ba9ae7
zhangjun <5024064@qq.com>
2021-08-01 10:27:41 +0800

    SpringTask(轻量级QuartZ): 6.3.1 基于配置文件实现SpirngTask任务调度处理 1. 定义任务类及方法 2. 配置文件task域，指定任务处理类及方法及调度时间（间隔或定时）

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
@@ -3,7 +3,7 @@ package cn.mldn.mldnspring.task;
 import java.text.SimpleDateFormat;
 
 public class MyTask {			// 不再强制性继承任何父类
-	protected void runTask() { 	// 随意定义的一个方法名称
+	public void runTask() { 	// 随意定义的一个方法名称
 		System.out.println("【当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new java.util.Date()));
 	}
 }
