46fe3b42a259dc950b4c423ad3be80920090b243
zhangjun <5024064@qq.com>
2021-08-09 22:14:23 +0800

    Spring的JDBC操作模板：11.3 Spring JDBC Template 操作数据库-通过KeyHolder获取新增后的ID（需要适配驱动，适应性差）

--- mldnspring-jdbc/src/test/java/cn/mldn/mldnspring/test/TestJdbcTemplate.java
+++ mldnspring-jdbc/src/test/java/cn/mldn/mldnspring/test/TestJdbcTemplate.java
@@ -1,5 +1,6 @@
 package cn.mldn.mldnspring.test;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Date;
@@ -9,7 +10,10 @@ import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -52,6 +56,29 @@ public class TestJdbcTemplate {
 		}) ; 
 		this.logger.info("更新行数：" + len);			// 日志输出
 	} 
-	
+	@Test
+	public void testAddKeyHolder() throws Exception {
+		KeyHolder keyHolder = new GeneratedKeyHolder() ;	// 获得增长后的ID数据
+		String sql = "INSERT INTO news(title,pubdate,note,price,readcount) VALUES (?,?,?,?,?)" ;
+		String title = "MLDN魔乐科技" ;
+		Date pubdate = new Date() ;
+		String note = "技术教学：www.mldn.cn" ;
+		double price = 19800.0 ;
+		int readcount = 567000 ;
+		int len = this.jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement(sql);
+				ps.setString(1, title);
+				ps.setDate(2, new java.sql.Date(pubdate.getTime()));
+				ps.setString(3, note);
+				ps.setDouble(4, price);
+				ps.setInt(5, readcount);
+				return ps;
+			}
+
+		}, keyHolder);
+		this.logger.info("更新行数：" + len + "、增长后的ID：" + keyHolder.getKey());			// 日志输出
+	} 
 	
 }
