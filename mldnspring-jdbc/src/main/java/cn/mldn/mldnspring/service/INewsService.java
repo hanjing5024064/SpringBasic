912a9762a9d35f0638b9cd8abe9e8f74d5f22397
zhangjun <5024064@qq.com>
2021-08-17 21:07:50 +0800

    Spring的JDBC操作模板：11.5 spring缓存实现 5. @CacheConfig(cacheNames="news")		// 进行该接口缓存的统一配置

--- mldnspring-jdbc/src/main/java/cn/mldn/mldnspring/service/INewsService.java
+++ mldnspring-jdbc/src/main/java/cn/mldn/mldnspring/service/INewsService.java
@@ -1,25 +1,27 @@
 package cn.mldn.mldnspring.service;
 
+import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
 import cn.mldn.mldnspring.vo.News;
 
+@CacheConfig(cacheNames="news")		// 进行该接口缓存的统一配置
 public interface INewsService {
 	/**
 	 * 根据ID查询数据
 	 * @param nid 新闻编号
 	 * @return 新闻数据对象
 	 */
-	@Cacheable(cacheNames = "news", key = "#nid", unless="#result==null")
+	@Cacheable(key = "#nid", unless="#result==null")
 	public News get(long nid) ;
 	/**
 	 * 数据更新操作
 	 * @param vo 要更新的数据
 	 * @return 更新后的数据
 	 */
-	@CachePut(cacheNames = "news", key = "#vo.nid", unless="#result==null")
+	@CachePut(key = "#vo.nid", unless="#result==null")
 	public News edit(News vo) ;  
 	
 	/**
@@ -27,6 +29,6 @@ public interface INewsService {
 	 * @param nid 新闻编号
 	 * @return 删除成功返回true，否则返回false
 	 */
-	@CacheEvict(cacheNames="news", key="#nid")
+	@CacheEvict(key="#nid")
 	public boolean delete(long nid) ;
 }
