dc97fe7f3db35cd8552656d0e888d2f069afd135
zhangjun <5024064@qq.com>
2021-08-21 16:55:57 +0800

    SpringDataJPA：13.3 JPA对象状态-4种状态 瞬时态对象New 	新的持久化对象，并未进行持久化存储，可能无ID无Persistence Context关联 	未关联对应数据库数据 持久态对象Managed 	已持久化，同时保存在一级缓存中，有Persistence Context关联；对其修改可能影响数据库中数据，需要进行事务提交； 	关联对应数据库数据 游离态对象Datached 	已持久化，有ID数据，无Persistence Context 关联（可能EntityManager关闭），对其修改不会影响到数据库中相关数据。 	不知道对应数据库哪个数据 删除态对象Removed 	与Persistence Context有关联，但是其对应数据库数据已经被删除。 	对应数据库数据已删除

--- mldnspring-jpa/src/test/java/cn/mldn/mldnspring/test/TestFirstCache.java
+++ mldnspring-jpa/src/test/java/cn/mldn/mldnspring/test/TestFirstCache.java
@@ -1,5 +1,7 @@
 package cn.mldn.mldnspring.test;
 
+import java.util.Date;
+
 import org.junit.Test;
 
 import cn.mldn.mldnspring.po.Dept;
@@ -8,14 +10,52 @@ import cn.mldn.mldnspring.util.JPAEntityFactory;
 public class TestFirstCache {
 	@Test
 	public void testFind() throws Exception {
-		Dept deptA = JPAEntityFactory.getEntityManager().find(Dept.class, 3L) ;
-		System.out.println("第一次使用find()查询：" + deptA);
-		deptA.setDname("修改第一次查询结果的部门名称");			// 修改数据
-		deptA.setNum(-1);									// 修改数据
-		System.out.println("--------------------- 华丽的分割线 --------------------");
-		JPAEntityFactory.getEntityManager().refresh(deptA); // 刷新缓存数据，否则还是上次查询后保存到缓存中的数据
-		Dept deptB = JPAEntityFactory.getEntityManager().find(Dept.class, 3L) ;
-		System.out.println("第二次使用find()查询：" + deptB);
+		// 通过find()方法查询出来的数据会自动保存在一级缓存之中，同时该对象状态也属于持久态
+		Dept dept = JPAEntityFactory.getEntityManager().find(Dept.class, 3L) ;
+		JPAEntityFactory.getEntityManager().getTransaction().begin();	// 开启事务
+		dept.setDname("MLDN教学研发中心");									// 修改持久态对象的属性
+		dept.setNum(30);												// 修改持久态对象的属性
+		JPAEntityFactory.getEntityManager().getTransaction().commit();	// 提交事务
+		JPAEntityFactory.close(); 										// 关闭连接
 	}
 
+	@Test
+	public void testBatch() { 
+		JPAEntityFactory.getEntityManager().getTransaction().begin();	// 开启事务
+		for (int x = 0 ; x < 1000 ; x ++) {
+			Dept dept = new Dept() ;									// 瞬时态对象
+			dept.setDname("极限IT教学部 - " + x);							// 设置数据
+			dept.setAvgsal(8968.88 + x); 								// 设置数据
+			dept.setCreatedate(new Date()); 							// 设置数据
+			dept.setNum(888 + x);										// 设置数据
+			JPAEntityFactory.getEntityManager().persist(dept); 			// 数据持久化，自动可以获取ID
+			if (x % 10 == 0) {											// 每10条记录清除一下缓存
+				JPAEntityFactory.getEntityManager().flush(); 			// 强制刷新
+				JPAEntityFactory.getEntityManager().clear(); 			// 清空缓存
+			}
+		}
+		JPAEntityFactory.getEntityManager().getTransaction().commit();	// 提交事务
+		JPAEntityFactory.close(); 										// 关闭连接
+	}
+
+	
+	@Test
+	public void testAdd() { 
+		Dept dept = new Dept() ;					// 创建持久态对象
+		dept.setDname("极限IT教学部");					// 设置属性
+		dept.setAvgsal(8968.88); 					// 设置属性
+		dept.setCreatedate(new Date()); 			// 设置属性
+		dept.setNum(888);							// 设置属性
+		JPAEntityFactory.getEntityManager().getTransaction().begin();	// 开启事务
+		// 数据持久化，对象将由瞬时态变为持久态（EntityManager关闭前），并且自动可以获取ID
+		JPAEntityFactory.getEntityManager().persist(dept); 				
+		JPAEntityFactory.getEntityManager().getTransaction().commit();	// 提交事务
+		System.out.println("*** 新增数据的ID是：" + dept.getDeptno());		// 自动获得当前增长后ID
+		// 将刚刚保存后的对象信息根据ID查询出来，由于一级缓存作用将不会向数据库发出查询指令
+		Dept selectDept = JPAEntityFactory.getEntityManager().find(Dept.class, dept.getDeptno()) ;
+		System.out.println(selectDept); 
+		JPAEntityFactory.close(); 					// 关闭连接
+	}
+
+	
 }
