248f664ed1c63732d7925a162c032ba52ab6cee1
zhangjun <5024064@qq.com>
2021-08-21 10:32:39 +0800

    SpringDataJPA：13.2 常用注解 @Entity 标注实体类 @Table 表名称与持久化表名称不一致可以特殊声明 @Column 列定义，一般自动映射，默认使用最大长度 @Temporal 时间类（DATE、TIME、TEMESTAMP）数据：@Temoral(TemporalType.DATE) @Transient 不生成持久化属性

--- mldnspring-jpa/src/main/java/cn/mldn/mldnspring/po/Dept.java
+++ mldnspring-jpa/src/main/java/cn/mldn/mldnspring/po/Dept.java
@@ -7,21 +7,32 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @SuppressWarnings("serial")
-@Entity													// 持久化类
+@Entity
 public class Dept implements Serializable {
 	@Id													// 主键列
-	@GeneratedValue(strategy = GenerationType.IDENTITY)	// 主键生成方式
-	private Long deptno;	// 字段的映射（属性名称=字段名称）
+	@TableGenerator(
+			name="DEPT_GENERATOR" ,						// 定义一个主键生成器的名称
+			table="table_id_generate" ,					// 负责生成主键的数据表名称
+			pkColumnName="id_key" ,						// 要获取的行信息
+			pkColumnValue="DEPT_ID" ,					// 获取指定一行的信息
+			valueColumnName="id_value" ,				// 主键操作的内容字段
+			allocationSize=1 )							// 每次增长的步长
+	@GeneratedValue(
+			strategy = GenerationType.TABLE, 
+			generator = "DEPT_GENERATOR")				// 根据名称引用配置的主键生成器
+	private Long deptno;								// 字段的映射（属性名称=字段名称） 
 	private double avgsal;
 	@Temporal(TemporalType.DATE)						// 类型描述
 	private Date createdate;
 	private String dname;
 	private int num;
-	// setter、getter略
+	// setter、getter略 
 	public Long getDeptno() {
 		return deptno;
 	}
