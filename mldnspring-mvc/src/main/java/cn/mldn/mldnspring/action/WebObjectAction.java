1e79662a3135621ce0cbbb6d5709a067959cc742
zhangjun <5024064@qq.com>
2021-08-28 15:07:23 +0800

    SpringMVC: 14.6 通过RequestContextHolder获取内置对象 http://localhost:8080/mldnspring-mvc/pages/web/show_two.action 控制台输出： INFO [cn.mldn.mldnspring.action.WebObjectAction] - *** 【REQUEST内置对象】ContextPath = /mldnspring-mvc INFO [cn.mldn.mldnspring.action.WebObjectAction] - *** 【APPLICATION内置对象】RealPath = /Users/zhangjun/Documents/workspace-spring-tool-suite-4-4.8.1.RELEASE/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/mldnspring-mvc/ INFO [cn.mldn.mldnspring.action.WebObjectAction] - *** 【RESPONSE内置对象】Locale = en_CN INFO [cn.mldn.mldnspring.action.WebObjectAction] - *** 【SESSION内置对象】SessionID = 80328C6EC7905D15707805DA2007A6F4

--- mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/WebObjectAction.java
+++ mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/WebObjectAction.java
@@ -1,22 +1,40 @@
 package cn.mldn.mldnspring.action;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 @Controller										// 定义控制器
 @RequestMapping("/pages/web/*")				// 定义该类的访问父路径，与方法中的路径进行组合为完整路径
 public class WebObjectAction {						// 自定义Action程序类
 	private Logger log = LoggerFactory.getLogger(WebObjectAction.class) ;	// 日志记录 
 	@RequestMapping("/show_one") 
-	public ModelAndView showObject(HttpServletRequest request, HttpServletResponse response) { 
+	public ModelAndView showOne(HttpServletRequest request, HttpServletResponse response) { 
 		this.log.info("*** 【REQUEST内置对象】ContextPath = " + request.getContextPath()) ;
 		this.log.info("*** 【APPLICATION内置对象】RealPath = " + request.getServletContext().getRealPath("/")) ;
 		this.log.info("*** 【RESPONSE内置对象】Locale = " + response.getLocale()) ;
 		this.log.info("*** 【SESSION内置对象】SessionID = " + request.getSession().getId()) ;
+		return null ;
+	}
+	
+	@RequestMapping("/show_two") 
+	public ModelAndView showTwo() { 
+		// 通过RequestContextHolder类可以获得HttpServletRequest与HttpServletResponse对象
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest() ;
+		HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse() ;
+		HttpSession session = request.getSession() ;
+		ServletContext context = request.getServletContext() ;
+		this.log.info("*** 【REQUEST内置对象】ContextPath = " + request.getContextPath()) ;
+		this.log.info("*** 【APPLICATION内置对象】RealPath = " + context.getRealPath("/")) ;
+		this.log.info("*** 【RESPONSE内置对象】Locale = " + response.getLocale()) ;
+		this.log.info("*** 【SESSION内置对象】SessionID = " + session.getId()) ;
 		return null ;
 	}
 }
