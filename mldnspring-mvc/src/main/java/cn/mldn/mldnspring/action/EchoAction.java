7d43f48823da94544ad39a5b36cfd77aae9c7b8d
zhangjun <5024064@qq.com>
2021-08-28 14:51:59 +0800

    SpringMVC: 14.3 SpringMVC接收多参数

--- mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EchoAction.java
+++ mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EchoAction.java
@@ -1,29 +1,41 @@
 package cn.mldn.mldnspring.action;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 @Controller										// 定义控制器
 @RequestMapping("/pages/message/*")				// 定义该类的访问父路径，与方法中的路径进行组合为完整路径
 public class EchoAction {						// 自定义Action程序类
 	private Logger log = LoggerFactory.getLogger(EchoAction.class) ;	// 日志记录 
-	@RequestMapping("/echo")					// 访问的路径为“echo.action”；
-	public ModelAndView echo(
-			@RequestParam(							// 表示对请求参数的配置
-			name = "msg", 							// 表示映射的请求参数的名称
-			required = false, 						// 表示该参数是否必须传递
-			defaultValue = "www.mldnjava.cn")			// 设置该参数为null时的默认值
-			String str) {							// 接收请求参数 
-		this.log.info("*** EchoAction接收到请求参数，msg = " + str);		// 日志输出
-		// ModelAndView主要功能是设置跳转路径以及进行request属性的保存
+	@PostMapping("/echo")					// 访问的路径为“echo.action”；
+	public ModelAndView echo(String msg, Integer level, String tags[], Date pubdate) { // 接收请求参数  
 		ModelAndView mav = new ModelAndView("/pages/message/message_show.jsp") ;
-		mav.addObject("echoMessage", "【ECHO】msg = " + str) ;			// 设置request数据
+		mav.addObject("echoMessage", "【ECHO】msg = " + msg) ;					// 设置request数据
+		mav.addObject("echoLevel", "【ECHO】level = " + level) ;					// 设置request数据
+		mav.addObject("echoTags", "【ECHO】tags = " + Arrays.toString(tags)) ;	// 设置request数据
+		mav.addObject("echoPubdate", "【ECHO】pubdate = " + pubdate) ;			// 设置request数据
 		return mav ;
 	}
-	@RequestMapping("/echo_pre")
+	@GetMapping("/echo_pre")
 	public String echoPre() {
 		return "/pages/message/message_input.jsp" ;
 	}
+	
+	@InitBinder 
+	public void initBinder(WebDataBinder binder) {	// 设置一个Web数据的绑定转换
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd") ;	// 定义转换处理
+		// 在WEB数据绑定之中注册一个自定义的规则绑定器，该操作主要是对java.util.Date类型处理，允许为null
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(sdf, true));
+	}
+
 }
--- mldnspring-mvc/src/main/webapp/pages/message/message_input.jsp
+++ mldnspring-mvc/src/main/webapp/pages/message/message_input.jsp
@@ -7,6 +7,15 @@
 %>
 <base href="<%=basePath%>" />
 <form action="<%=message_input_url%>" method="post">
-	请输入消息：<input type="text" name="msg" id="msg" value="www.mldn.cn">
-	<input type="submit" value="发送">
-</form>
+	消息内容：	<input type="text" name="msg" id="msg" value="www.mldn.cn"><br>
+	消息级别：	<select id="level" name="level">
+				<option value="0">紧急</option>
+				<option value="1">普通</option>
+				<option value="2">延迟</option>
+			</select><br>
+	发送日期：	<input type="text" id="pubdate" name="pubdate" value="2262-01-21"><br>
+	消息标签：	<input type="checkbox" name="tags" id="tags" value="政治">政治
+			<input type="checkbox" name="tags" id="tags" value="经济">经济
+			<input type="checkbox" name="tags" id="tags" value="文化">文化<br>
+	<input type="submit" value="发送">	<input type="reset" value="重置">
+</form> 
