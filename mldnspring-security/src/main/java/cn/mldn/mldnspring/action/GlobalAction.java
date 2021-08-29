d8908b9286025c16afd1e412043d5c0df64cf15c
zhangjun <5024064@qq.com>
2021-08-29 16:34:33 +0800

    SpringSecurity: 15.4 欢迎页面获取用户授权信息 http://localhost:8080/mldnspring-mvc/logoutPage.action

--- mldnspring-security/src/main/java/cn/mldn/mldnspring/action/GlobalAction.java
+++ mldnspring-security/src/main/java/cn/mldn/mldnspring/action/GlobalAction.java
@@ -1,17 +1,39 @@
 package cn.mldn.mldnspring.action;
+import java.util.Collection;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 @Controller								// 定义控制器
 public class GlobalAction {				// 定义全局Action类
+	private Logger log = LoggerFactory.getLogger(EchoAction.class) ;	// 日志记录 
 	@RequestMapping("/loginPage")				// 访问路径
 	public String login() {				// 登录表单路径
 		return "login";					// 设置跳转路径
 	}
-	@RequestMapping("/welcomePage")				// 访问路径
-	public String welcome() {			// 登录成功路径
-		return "welcome";				// 设置跳转路径
-	}
+	@RequestMapping("/welcomePage")								// 访问路径
+	public String welcome() {									// 登录成功路径
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication() ;	// 认证对象
+		UserDetails userDetails = (UserDetails) authentication.getPrincipal() ;	// 获取用户详情
+		String username = userDetails.getUsername() ; 			// 获得用户名
+		this.log.info("用户名：" + username);
+		Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities() ; // 获取授权信息
+		this.log.info("授权信息：" + authorities);
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
+				.getRequestAttributes()).getRequest() ;			// 获取request内置对象
+		boolean isAdminRole = request.isUserInRole("ADMIN");	// 判断是否有指定授权信息
+		this.log.info("是否具有“ADMIN”角色：" + isAdminRole); 
+		return "welcome";										// 设置跳转路径
+	} 
 	@RequestMapping("/logoutPage")				// 访问路径
 	public String logout() {			// 登录成功路径
 		return "logout";				// 设置跳转路径
--- mldnspring-security/src/main/webapp/WEB-INF/pages/welcome.jsp
+++ mldnspring-security/src/main/webapp/WEB-INF/pages/welcome.jsp
@@ -1,4 +1,5 @@
 <%@ page pageEncoding="UTF-8"%>
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <html>
 <head>
 <%
@@ -11,7 +12,17 @@
 <base href="<%=basePath%>" />
 </head>
 <body>
-<h2>登录成功，欢迎您回来，也可以选择<a href="<%=logout_url%>">注销</a>！</h2>
+<security:authorize access="isAuthenticated()">		<!-- 是否为认证过的用户 -->
+	用户已经成功登录了！
+</security:authorize>
+<security:authorize access="hasRole('USER')">		<!-- 是否拥有“USER”角色 -->
+	拥有USER角色
+</security:authorize>
+<security:authorize access="hasRole('ADMIN')">		<!-- 是否拥有“ADMIN”角色 -->
+	拥有ADMIN角色
+</security:authorize>
+<h2>登录成功，欢迎“<security:authentication property="principal.username"/>”回来，
+也可以选择<a href="<%=logout_url%>">注销</a>！</h2>
 <h3>更多内容请访问<a href="http://www.mldn.cn">MLDN-魔乐科技</a></h3>
 <img src="mvcimages/mldn.png" style="width:500px;">
 <img src="mvcimages/jixianit.png" style="width:500px;">
