22cfd85ac6cffa30ccf5bc736ea248f299cfae4c
zhangjun <5024064@qq.com>
2021-08-29 09:14:00 +0800

    SpringMVC: 14.11 拦截器：前端验证 MessageSource读取验证规则-拦截器拦截add请求进行提交参数的验证 INFO [cn.mldn.util.ValidationInterceptor] - 【验证规则 - ｛/mldnspring-mvc/pages/emp/add.action｝】empno:long|ename:string|salary:double|hiredate:date|dept.deptno:long|dept.dname:string|dept.createdate:date INFO [cn.mldn.mldnspring.action.EmpAction] - Emp [empno=7369, ename=李兴华, salary=3000.33, hiredate=Mon Nov 11 00:00:00 CST 1991, level=0]
    http://localhost:8080/mldnspring-mvc/pages/emp/add_pre.action
    http://localhost:8080/mldnspring-mvc/pages/emp/add.action

--- mldnspring-mvc/src/main/java/cn/mldn/util/ValidationInterceptor.java
+++ mldnspring-mvc/src/main/java/cn/mldn/util/ValidationInterceptor.java
@@ -3,27 +3,35 @@ package cn.mldn.util;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 public class ValidationInterceptor implements HandlerInterceptor {	// 定义拦截器
 	private Logger logger = LoggerFactory.getLogger(ValidationInterceptor.class) ; 
+	
+	@Autowired
+	private MessageSource messageSource ;		// 获取资源数据
+
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		this.logger.info("1、preHandle()方法执行，" + handler.getClass());
+		if (handler instanceof HandlerMethod) { // 执行向下转型前应该首先判断其是否是指定类的实例
+			HandlerMethod handlerMethod = (HandlerMethod) handler; // 强制转换
+			String validationRuleKey = handlerMethod.getBeanType().getName() + "." + handlerMethod.getMethod().getName() ;
+			String validationRule = null ; // 保存要读取指定的资源key对应的验证规则
+			try {							// 如果指定的key不存在，表示现在不需要进行验证
+				validationRule = this.messageSource.getMessage(validationRuleKey, null, null) ;
+			} catch (Exception e) {}
+			if (validationRule != null) {	// 验证处理操作，则需要进行验证处理
+				this.logger.info("【验证规则 - ｛"+request.getRequestURI()+"｝】" + validationRule); 
+			}
+		}
 		return true ;	// 返回true表示请求继续，而如果返回了false表示不执行后续的Action或拦截器
 	}
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		this.logger.info("2、postHandle()方法执行，" + handler.getClass() + "、ModelAndView = " + modelAndView);
-	}
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		this.logger.info("3、afterCompletion()方法执行，" + handler.getClass());
-	} 
 }
